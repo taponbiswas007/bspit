1b58351c33a4e814675d7d41145ba51645b66fdb
Tapon chandra sarker <108454446+taponbiswas007@users.noreply.github.com>
2024-04-19 14:56:19 +0600

    button border update

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,16 @@ $(document).ready(function(){
             "left": "0",
             "right": "100%" 
         });
-        $(".box-shadow").css("transition", "left 0.5s, top 0.5s, bottom 0.5s, right 0.5s"); // Add transition
+        $(".box-shadow").css("transition", " bottom 0.5s, right 0.5s"); // Add transition
         $(".box-shadow").css({
-            "left": "0",
-            "top": "0" ,
             "bottom": "0", 
             "right": "0" ,
-            "border": "1px solid #ffffff"
+           
+        });
+        $(".hover-box").css("transition", " all 0.5s"); // Add transition
+        $(".hover-box").css({
+            "border": "1px solid #ffffff",
+           
         });
     }, function(){
         $(".default-bg").css("transition", "left 0.5s, right 0.5s"); // Reset transition
@@ -19,14 +22,17 @@ $(document).ready(function(){
             "left": "0", // Reset left position
             "right": "0" // Reset right position
         });
-        $(".box-shadow").css("transition",  "left 0.5s, top 0.5s, bottom 0.5s, right 0.5s"); // Add transition
+        $(".box-shadow").css("transition",  "bottom 0.5s, right 0.5s"); // Add transition
         $(".box-shadow").css({
-            "left": "4px",
-            "top": "4px" ,
             "bottom": "-4px", 
             "right": "-4px" ,
             "border-right": "4px solid #ffffff",
             "border-bottom": "4px solid #ffffff",
+        });
+        $(".hover-box").css("transition", " all 0.5s"); // Add transition
+        $(".hover-box").css({
+            "border": "1px solid #000000",
+           
         });
     });
 });
