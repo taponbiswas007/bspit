3b23069fa8caec13a014eccdc4b5875421bb70c4
Tapon chandra sarker <108454446+taponbiswas007@users.noreply.github.com>
2024-04-19 16:37:10 +0600

    dfds

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,62 +1,74 @@
-$(document).ready(function(){
-    $(".contact-button").hover(function(){
-        $(".default-bg").css("transition", "left 0.5s, right 0.5s"); // Add transition
+$(document).ready(function () {
+    $(".contact-button").hover(function () {
+        $(".default-bg").css("transition", "all 0.6s"); // Add transition
         $(".default-bg").css({
+            "transition-delay": "0.4s",
             "left": "0",
-            "right": "100%" 
+            "right": "100%"
         });
-        $(".box-shadow").css("transition", " bottom 0.5s, right 0.5s"); // Add transition
+        $(".box-shadow").css("transition", " all 0.5s"); // Add transition
         $(".box-shadow").css({
-            "bottom": "0", 
-            "right": "0" ,
-           
+            "bottom": "0",
+            "right": "0",
+
+
         });
         $(".hover-box").css("transition", " all 0.5s"); // Add transition
         $(".hover-box").css({
+
             "border": "1px solid #ffffff",
-           
+
+
         });
-        $(".hover-text").css("transition", " top 0.5s"); // Add transition
+        $(".hover-text").css("transition", "top 0.5s"); // Add transition
         $(".hover-text").css({
-            "display": "block",
-            "z-index": "4"
-           
+            "transition-delay":"0.5s",
+            "top": "50%",
+            "left": "50%",
+            "transform": "translate(-50%, -50%)", // Combine translations here
+            "z-index": "4",
         });
-        $("defaulttext").css("transition", " top 0.5s"); // Add transition
-        $("defaulttext").css({
-            "display": "none",
-           
-           
+
+        $(".default-text").css("transition", "top 0.5s"); // Add transition
+        $(".default-text").css({
+            "top": "100%",
+            "transform": "translate(-50%, 0)"
         });
-    }, function(){
-        $(".default-bg").css("transition", "left 0.5s, right 0.5s"); // Reset transition
+        
+    }, function () {
+        $(".default-bg").css("transition", "all 0.6s"); // Reset transition
         $(".default-bg").css({
             "left": "0", // Reset left position
             "right": "0" // Reset right position
         });
-        $(".box-shadow").css("transition",  "bottom 0.5s, right 0.5s"); // Add transition
+        $(".box-shadow").css("transition", "all 0.5s"); // Add transition
         $(".box-shadow").css({
-            "bottom": "-4px", 
-            "right": "-4px" ,
+            "bottom": "-4px",
+            "right": "-4px",
             "border-right": "4px solid #ffffff",
             "border-bottom": "4px solid #ffffff",
         });
         $(".hover-box").css("transition", " all 0.5s"); // Add transition
         $(".hover-box").css({
-            "border": "1px solid #000000",
-           
+            "border": "2px solid #131313",
+
+
         });
         $(".hover-text").css("transition", " top 0.5s"); // Add transition
         $(".hover-text").css({
-            "display": "none",
-            "z-index": "4"
-           
+            "top": "-100%",
+            "left": "50%",
+            
+
         });
-        $("defaulttext").css("transition", " top 0.5s"); // Add transition
-        $("defaulttext").css({
-            "display": "block",
-           
-           
+        $(".default-text").css("transition", " top 0.5s"); // Add transition
+        $(".default-text").css({
+            "top": "50%",
+            "left": "50%",
+            "transform": "translate(-50%, -50%)", // Combine translations here
+            "z-index": "4",
+
+
         });
     });
 });
