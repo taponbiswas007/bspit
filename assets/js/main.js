89eaaddd4b1784c37fa83ac66d059cb753bbcc50
taponbiswas007 <taponbiswassdp@gmail.com>
2024-04-29 14:24:29 +0600

    about

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,6 +65,71 @@ $(document).ready(function () {
             "z-index": "4",
         });
     });
+    $(".contact-buttonwhite").hover(function () {
+        var $button = $(this); // Reference to the hovered button
+    
+        // Target only the elements within the hovered button
+        $button.find(".default-bg").css({
+            "transition": "all 0.6s",
+            "transition-delay": "0.4s",
+            "left": "0",
+            "right": "100%"
+        });
+        $button.find(".box-shadow").css({
+            "transition": "all 0.5s",
+            "bottom": "0",
+            "right": "0",
+        });
+        $button.find(".hover-box").css({
+            "transition": "all 0.5s",
+            "border": "1px solid #111",
+        });
+        $button.find(".hover-text").css({
+            "transition": "top 0.2s",
+            "transition-delay": "0.5s",
+            "top": "50%",
+            "left": "50%",
+            "transform": "translate(-50%, -50%)",
+            "z-index": "4",
+        });
+        $button.find(".default-text").css({
+            "transition": "top 0.2s",
+            "top": "130%",
+            "transform": "translate(-50%, 0)"
+        });
+    }, function () {
+        var $button = $(this); // Reference to the hovered button
+    
+        // Reset the styles for elements within the hovered button
+        $button.find(".default-bg").css({
+            "transition": "all 0.6s",
+            "left": "1px",
+            "right": "1px",
+        });
+        $button.find(".box-shadow").css({
+            "transition": "all 0.5s",
+            "bottom": "-4px",
+            "right": "-4px",
+            "border-right": "4px solid #111",
+            "border-bottom": "4px solid #111",
+        });
+        $button.find(".hover-box").css({
+            "transition": "all 0.5s",
+            "border": "2px solid #ffffff",
+        });
+        $button.find(".hover-text").css({
+            "transition": "top 0.2s",
+            "top": "-100%",
+            "left": "50%",
+        });
+        $button.find(".default-text").css({
+            "transition": "top 0.2s",
+            "top": "50%",
+            "left": "50%",
+            "transform": "translate(-50%, -50%)",
+            "z-index": "4",
+        });
+    });
     //contact button area end    
 
     //welcome screen start
